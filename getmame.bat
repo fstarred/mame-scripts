1c380e08e4ed712d51d05a2d5ea0a8f5f103daad
Fabrizio Stellato <fabrizio.stellato@gmail.com>
2023-03-23 15:11:59 +0100

    Update getmame.bat

--- getmame.bat
+++ getmame.bat
@@ -1,47 +1,40 @@
 @echo off
 set url=https://github.com/mamedev/mame/releases/download
-set outdir=D:\Emulators
-set curdir=d:\emulators\mame
-set zipcmd=7za.exe
+set mamebasedir=D:\Emulators
+set mameworkdir=d:\emulators\mame
+set zipcmd=7zr.exe
 set luadir=d:\lua
-set luaexe=lua53.exe
+set luaexe=lua54.exe
 
 if "%~1"=="" goto errparam
 
 set url=%url%/mame%1
 set file=mame%1b_64bit.exe
 
-if not exist %outdir%\%file% (
+if not exist %mamebasedir%\%file% (
 	echo downloading mame %1...
-	powershell.exe -command "& Invoke-WebRequest -Uri %url%/%file% -O %outdir%\%file% -UseBasicParsing"
+	powershell.exe -command "& Invoke-WebRequest -Uri %url%/%file% -O %mamebasedir%\%file% -UseBasicParsing"
 ) 
 
 echo extracting...
 
-%zipcmd% x %outdir%\%file% -o%outdir%\mame%1 -r
+%zipcmd% x %mamebasedir%\%file% -o%mamebasedir%\mame%1 -r -aos
 
 echo creating link to public desktop
 
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%PUBLIC%\Desktop\mame.lnk');$s.WorkingDirectory='%outdir%\mame%1';$s.TargetPath='%outdir%\mame%1%\mame.exe';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%PUBLIC%\Desktop\mame.lnk');$s.WorkingDirectory='%mamebasedir%\mame%1';$s.TargetPath='%mamebasedir%\mame%1%\mame.exe';$s.Save()"
 
 echo creating link to current dir
 
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%curdir%\mame.lnk');$s.WorkingDirectory='%outdir%\mame%1';$s.TargetPath='%outdir%\mame%1%\mame.exe';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%mameworkdir%\mame.lnk');$s.WorkingDirectory='%mamebasedir%\mame%1';$s.TargetPath='%mamebasedir%\mame%1%\mame.exe';$s.Save()"
 
 echo generating mame ini files
 
-start /D %outdir%\mame%1 /WAIT /B mame.exe -cc
+start /D %mamebasedir%\mame%1 /WAIT /B mame.exe -cc
 
 echo modifying mame ini values
 
-%luadir%\%luaexe% %curdir%\set_ini.lua %1
-
-if exist %curdir%\default.cfg (
-	echo copying default.cfg...
-	copy %curdir%\default.cfg %outdir%\mame%1\cfg\
-) else (
-	echo warning: no default.cfg file found
-)
+%luadir%\%luaexe% .\set_ini.lua %1
 
 :exitprog
 exit /B 0
