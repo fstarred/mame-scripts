fe3bf125a76200c9e1144a2ba3eaa33eec7964dc
Fabrizio Stellato <fabrizio.stellato@gmail.com>
2023-03-23 15:12:54 +0100

    Update paths.lua

--- paths.lua
+++ paths.lua
@@ -3,7 +3,8 @@ mame_basedir = "d:\\emulators"
 
 tbl_mame_ini = {
     rompath = "d:\\emulators\\mame\\roms", 
-    snapshot_directory = "d:\\emulators\\mame\\snap"
+    snapshot_directory = "d:\\emulators\\mame\\snap",
+	cfg_directory = "d:\\emulators\\mame\\cfg"
 }
 
 tbl_ui_ini = {
