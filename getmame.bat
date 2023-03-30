9e144ff6d178905975a507d07759c474af07f3a7
Fabrizio Stellato <fabrizio.stellato@gmail.com>
2023-03-30 10:22:57 +0200

    Update getmame.bat

--- getmame.bat
+++ getmame.bat
@@ -6,6 +6,7 @@ set mameworkdir=d:\emulators\mame
 set zipcmd=7zr.exe
 set luadir=d:\lua
 set luaexe=lua54.exe
+set workingdir=%~dp0
 
 if "%~1"=="" goto errparam
 
@@ -19,7 +20,7 @@ if not exist %downloaddir% (
 if not exist %downloaddir%\%file% (
 	echo downloading mame %1...
 	powershell.exe -command "& Invoke-WebRequest -Uri %url%/%file% -O %downloaddir%\%file% -UseBasicParsing"
-) 
+)
 
 echo extracting...
 
@@ -29,9 +30,14 @@ echo creating link to public desktop
 
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%PUBLIC%\Desktop\mame.lnk');$s.WorkingDirectory='%mamebasedir%\mame%1';$s.TargetPath='%mamebasedir%\mame%1%\mame.exe';$s.Save()"
 
-echo creating link to current dir
+echo creating link to mame work directory
 
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%mameworkdir%\mame.lnk');$s.WorkingDirectory='%mamebasedir%\mame%1';$s.TargetPath='%mamebasedir%\mame%1%\mame.exe';$s.Save()"
+
+echo creating link to current directory
+
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%workingdir%mame.lnk');$s.WorkingDirectory='%mamebasedir%\mame%1';$s.TargetPath='%mamebasedir%\mame%1%\mame.exe';$s.Save()"
+
 
 echo generating mame ini files
 
@@ -39,7 +45,7 @@ start /D %mamebasedir%\mame%1 /WAIT /B mame.exe -cc
 
 echo modifying mame ini values
 
-%luadir%\%luaexe% .\set_ini.lua %1
+%luadir%\%luaexe% %workingdir%set_ini.lua %1
 
 :exitprog
 exit /B 0
@@ -47,3 +53,4 @@ exit /B 0
 :errparam
 echo "usage: %~n0 <newversion> (ex. %~n0 0232)"
 exit /B 1
+
