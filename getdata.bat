e068e577c0d318ff3b0441236849f4949342abce
Fabrizio Stellato <fabrizio.stellato@gmail.com>
2023-04-12 14:57:04 +0200

    Update getdata.bat

--- getdata.bat
+++ getdata.bat
@@ -11,30 +11,50 @@ if "%~1"=="" goto errparam
 
 echo downloading snap data..
 
-if not exist %snap_dir% (
-	mkdir %snap_dir%
-)
+call :createdir %snap_dir%
 
 powershell.exe -command "& Invoke-WebRequest -Uri %snap_url%%1%fileext% -O %snap_dir%\%1%fileext% -UseBasicParsing"
 
+call :checksize %snap_dir%\%1%fileext%
+
+
+
+
 echo downloading cabinets data..
 
-if not exist %cabinets_dir% (
-	mkdir %cabinets_dir%
-)
+call :createdir %cabinets_dir%
 
 powershell.exe -command "& Invoke-WebRequest -Uri %cabinets_url%%1%fileext% -O %cabinets_dir%\%1%fileext% -UseBasicParsing"
 
+call :checksize %cabinets_dir%\%1%fileext%
+
+
+
+
 echo downloading cpanel data..
 
-if not exist %cpanels_dir% (
-	mkdir %cpanels_dir%
-)
+call :createdir %cpanels_dir%
 
 powershell.exe -command "& Invoke-WebRequest -Uri %cpanel_url%%1%fileext% -O %cpanels_dir%\%1%fileext% -UseBasicParsing"
+
+call :checksize %cpanels_dir%\%1%fileext%
 
 exit /B 0
 
 :errparam
 echo "usage: %~n0 <romname>"
 exit /B 1
+
+:checksize
+set size=%~z1
+if %size% == 0 (
+	echo file non valid, removing..
+	del %1
+)
+goto :eof
+
+:createdir
+if not exist %1 (
+	mkdir %1
+)
+goto :eof
