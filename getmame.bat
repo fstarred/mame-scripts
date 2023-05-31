b9b9b84c84b89a5222230acca06a8aaecc44875f
Fabrizio Stellato <fabrizio.stellato@gmail.com>
2023-05-31 09:12:46 +0200

    Update getmame.bat

--- getmame.bat
+++ getmame.bat
@@ -12,6 +12,7 @@ if "%~1"=="" goto errparam
 
 set url=%url%/mame%1
 set file=mame%1b_64bit.exe
+set curmamedir=%mamebasedir%\mame%1
 
 if not exist %downloaddir% (
 	mkdir %downloaddir%
@@ -24,28 +25,32 @@ if not exist %downloaddir%\%file% (
 
 echo extracting...
 
-%zipcmd% x %downloaddir%\%file% -o%mamebasedir%\mame%1 -r -aos
+%zipcmd% x %downloaddir%\%file% -o%curmamedir% -r -aos
 
 echo creating link to public desktop
 
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%PUBLIC%\Desktop\mame.lnk');$s.WorkingDirectory='%mamebasedir%\mame%1';$s.TargetPath='%mamebasedir%\mame%1%\mame.exe';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%PUBLIC%\Desktop\mame.lnk');$s.WorkingDirectory='%curmamedir%';$s.TargetPath='%curmamedir%\mame.exe';$s.Save()"
 
 echo creating link to mame work directory
 
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%mameworkdir%\mame.lnk');$s.WorkingDirectory='%mamebasedir%\mame%1';$s.TargetPath='%mamebasedir%\mame%1%\mame.exe';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%mameworkdir%\mame.lnk');$s.WorkingDirectory='%curmamedir%';$s.TargetPath='%curmamedir%\mame.exe';$s.Save()"
 
 echo creating link to current directory
 
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%workingdir%mame.lnk');$s.WorkingDirectory='%mamebasedir%\mame%1';$s.TargetPath='%mamebasedir%\mame%1%\mame.exe';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%workingdir%mame.lnk');$s.WorkingDirectory='%curmamedir%';$s.TargetPath='%curmamedir%\mame.exe';$s.Save()"
 
 
 echo generating mame ini files
 
-start /D %mamebasedir%\mame%1 /WAIT /B mame.exe -cc
+start /D %curmamedir% /WAIT /B mame.exe -cc
 
 echo modifying mame ini values
 
 %luadir%\%luaexe% %workingdir%set_ini.lua %1
+
+echo copying ini presets to mameworkdir ini directory
+
+copy /Y %curmamedir%\ini\presets\* %mameworkdir%\ini\presets\ 1> NUL
 
 :exitprog
 exit /B 0
@@ -53,4 +58,5 @@ exit /B 0
 :errparam
 echo "usage: %~n0 <newversion> (ex. %~n0 0232)"
 exit /B 1
+
 
